7e0abeb92d31123afb3aad003dc57db4df7cded0
sergioss911 <43449771+sergioss911@users.noreply.github.com>
2018-11-29 10:51:21 -0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,169 @@
+var myImages =["Html_Validator.PNG","CSS_Validator.PNG","Speed_Validator.PNG "];
+
+
+
+
+
+
+
+
+
+
+
+var captionImages =["HTML","CSS","SPEED"];
+
+
+
+
+
+
+
+var index=0; 
+
+
+
+
+
+
+
+function updateImage(){
+
+
+
+  document.getElementById("slideshow").src = myImages[index];
+
+
+
+  document.getElementById("slideshow").alt= captionImages[index];
+
+
+
+  document.getElementById("caption").textContent = captionImages[index]; 
+
+
+
+} 
+
+
+
+
+
+
+
+function next(){
+
+
+
+  if (myImages.length == index+1)
+
+
+
+    index=0;
+
+
+
+  else
+
+
+
+    index++;
+
+
+
+  updateImage();
+
+
+
+} 
+
+
+
+
+
+
+
+
+
+
+
+function back(){
+
+
+
+  if (index===0)
+
+
+
+    index=myImages.length-1;
+
+
+
+  else
+
+
+
+    index--;
+
+
+
+
+
+
+
+  updateImage();
+
+
+
+} 
+
+
+
+
+
+
+
+var nextButton = document.getElementById("next"); 
+
+
+
+var previousButton = document.getElementById("previous"); 
+
+
+
+
+
+
+
+previousButton.addEventListener("click",back,false);
+
+
+
+nextButton.addEventListener("click",next,false); 
+
+
+
+function autoSlide(){
+
+
+
+  if (document.getElementById("auto").checked)
+
+
+
+    next(); 
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+setInterval(autoSlide,2000); // Next
